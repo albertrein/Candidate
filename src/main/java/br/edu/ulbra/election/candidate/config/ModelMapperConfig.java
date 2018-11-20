09f9e71f8ea018662c938fb5f762c02383e25f4f
albertrein <guilhermealberto8@gmail.com>
2018-11-19 22:28:43 -0200

    Alterando Candidate para v2.0 do professor.

--- src/main/java/br/edu/ulbra/election/candidate/config/ModelMapperConfig.java
+++ src/main/java/br/edu/ulbra/election/candidate/config/ModelMapperConfig.java
@@ -1,4 +1,5 @@
 package br.edu.ulbra.election.candidate.config;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,9 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setAmbiguityIgnored(true);
+        return modelMapper;
     }
+
 }
--- src/main/java/br/edu/ulbra/election/candidate/config/SwaggerConfig.java
+++ src/main/java/br/edu/ulbra/election/candidate/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package br.edu.ulbra.election.candidate.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
